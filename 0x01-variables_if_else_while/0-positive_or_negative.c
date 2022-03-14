b615ed55b127aea697d35dbf8ccc60f9464d045e
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-14 09:53:50 -0700

    Added c program to determine the sign of a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if(n == 0)
+	{ 
+		printf("%d is zero\n", n);
+	}
+	else if(n > 0)
+	{
+		printf("%d is greater than 0\n", n);
+	}
+	else 
+	{
+		printf("%d is less than 0\n", n);
+	}
+	/* your code goes there */
+	return (0);
+}
