2d48ce9466f18adf5936e7f732fa2f70e19f51dc
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 03:45:18 -0700

    updated this file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,9 @@ int main(void)
         char str[] = "_putchar";
         long unsigned int i;
 
-        for(i = 0; i < strlen(str); i++){
-
-                _putchar(str[i]);
-        }
+        for(i = 0; i < strlen(str); i++)
+		_putchar(str[i]);
+        
         _putchar('\n');
 
         return 0;
