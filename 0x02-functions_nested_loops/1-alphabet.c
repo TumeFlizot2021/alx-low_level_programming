4a28f513874af789694e2d0948cb087118ffa7a2
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 04:41:53 -0700

    Added c file to print alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+void print_alphabet(void)
+{
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+                _putchar(c);
+                c = c + 1;
+        }
+_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+ 
+//this  function prints a character at a time
 	int _putchar(char c);
 
+//function prints alphabets in lower case
+	void print_alphabet(void);
 #endif
