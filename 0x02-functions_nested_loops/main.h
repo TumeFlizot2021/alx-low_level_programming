6360f294a4adffe14d32cfa6e211440299d6aa7b
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-16 12:46:09 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
  
-//this  function prints a character at a time
 	int _putchar(char c);
 
-//function prints alphabets in lower case
 	void print_alphabet(void);
 #endif
