1ee2f001372063861036eb4027db17538059189c
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 07:38:49 -0700

    Added c program to test calculate absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs- check the code.
+ *@a: letter is being tested
+ * Return: Either absolute value of a.
+ */
+
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+else
+{
+return (-(a));
+}
+}
