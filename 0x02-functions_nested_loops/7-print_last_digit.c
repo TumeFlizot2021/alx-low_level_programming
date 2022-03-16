23c9d29cd3d9a6d6079e4aca85e4b57183274c93
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 14:04:48 -0700

    Updated this program

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,18 +5,14 @@
  * Return: Emodullo of a.
  */
 
-int print_last_digit(int a)
+int print_last_digit(int n)
 {
-int b;
-if (a > 0)
+n = n % 10;
+
+if (n < 0)
 {
-b = a % 10;
+n = -n;
+putchar(n + '0');
 }
-else
-{
-b = -(a);
-b = b % 10;	
-}
-_putchar('0' + b);
-return (b);
+return (n);
 }
