b6e5e282bf228de500b2dfe2c88c2934990a4765
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-16 12:06:41 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,16 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 char str[] = "_putchar";
-long unsigned int i;
+unsigned long int i;
 for (i = 0; i < strlen(str); i++)
 {
 _putchar (str[i]);
