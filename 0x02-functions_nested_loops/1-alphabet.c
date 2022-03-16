6b1fa9bda8cce72f0ed613de9033408f803fa298
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 04:58:15 -0700

    Added c file to print alphabets in lower case from a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
-
+#include "main.h"
+/**
+ * print_aphabet - prints alphabets from a to z.
+ *
+ * Return: nothing.
+ */
 void print_alphabet(void)
 {
-        char c = 'a';
-
-        while (c <= 'z')
-        {
-                _putchar(c);
-                c = c + 1;
-        }
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c = c + 1;
+}
 _putchar('\n');
 }
 
