28f4144c89658be4092d701b31d233f7af972b7c
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-16 05:49:00 -0700

    Added a c program to print ten lines of alphabet a-z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always nothing.
+ */
+void print_alphabet_x10(void)
+{
+char c = 'a';
+int i;
+for (i = 0; i < 10; i++)
+{
+while(c <= 'z')
+{
+_putchar(c);
+c = c + 1;
+}
+c = 'a';
+_putchar('\n');
+}
+
+}
