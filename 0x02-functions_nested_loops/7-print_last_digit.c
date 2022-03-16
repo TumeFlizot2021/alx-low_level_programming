97ca9898e97f6aa4ef76fd99243b1edc5b46bd07
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-16 16:32:59 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,16 @@
 
 int print_last_digit(int a)
 {
- int b;
- a = a >= 0 ? a : a * -1;
- b = a * 10;
+int b;
+if (a > 0)
+{
+b = a % 10;
+}
+else
+{
+b = -(a);
+b = b % 10;	
+}
 _putchar('0' + b);
 return (b);
 }
