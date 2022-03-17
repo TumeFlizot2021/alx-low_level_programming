7665ea977820b5e0d20205f981448b3182020db8
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-17 09:40:21 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * print_last_digit- check the code.
- *@n: letter is being tested
- * Return: Emodullo of a.
+ * jack_bauer- check the code.
+ * Return: time.
  */
 
 void jack_bauer(void)
 {
 int hr, min;
-for (hr = 0; hr < 24; hr++)
+for(hr = 0; hr < 24; hr++)
 {
- for (min = 0; min < 60; min++)
+for(min = 0; min < 60; min++)
 {
 _putchar((hr / 10) + '0');
 _putchar((hr % 10) + '0');
