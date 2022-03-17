616342786c341be6b6d8f06ca32964b75a0c61e7
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-17 07:17:44 -0700

    Added c file to print multiples of 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table- check the code.
+ * Return: the multiples of 0 - 9.
+ */
+ 
+
+void times_table(void)
+{
+int i, j;
+int temp;
+for (i = 0; i < 10; i++)
+{
+_putchar('0');
+temp = i;
+for (j = 1; j < 10; j++)
+{
+if(temp <= 9)
+{
+_putchar(' ');
+_putchar('0' + temp);
+}
+else
+{
+_putchar(temp / 10 + '0');
+_putchar(temp % 10 + '0');
+}
+if(j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+temp = temp + i;
+}
+_putchar('\n');
+}
+}
