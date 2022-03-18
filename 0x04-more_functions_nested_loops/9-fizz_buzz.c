8b2cfdc2a2c3d44633b44cf9f01f14249adcf4c1
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 20:29:30 +0100

    Added fizzbuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,29 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
-	{
-		if ((i % 3 == 0) && ( i % 5 != 0))
-		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if ((i % 5 ==0) && (i % 3 != 0))
-		{
-			printf("Buzz");
-			printf(" ");
-		}
-		else if ((i % 5 == 0) && (i % 3 == 0))
-		{
-			printf("FizzBuzz");
-			printf(" ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
-	return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && ( i % 5 != 0))
+{
+printf("Fizz");
+printf(" ");
+}
+else if ((i % 5 ==0) && (i % 3 != 0))
+{
+printf("Buzz");
+printf(" ");
+}
+else if ((i % 5 == 0) && (i % 3 == 0))
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
 }
