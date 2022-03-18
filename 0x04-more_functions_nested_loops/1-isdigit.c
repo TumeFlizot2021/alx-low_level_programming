59b8cde315dc3f07e730ff969c7815d984296d83
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-18 08:19:25 -0700

    Added is digit program

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
