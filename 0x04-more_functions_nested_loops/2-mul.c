342c2317c5e2ff8414c4a729fce3412b5c150dff
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-18 08:31:28 -0700

    Added a program to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mult - check for upper case letter
+ * @a : number to multiply
+ * @b : number to multiply
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
