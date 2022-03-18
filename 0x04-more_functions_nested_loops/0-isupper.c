6f93da7eec84718dd93eda403bf793e1337d6e0f
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 16:12:37 +0100

    Added is upper program

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - check the code.
- * @c: is checked 
- * 
- * Return: Always 0 or 1.
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
