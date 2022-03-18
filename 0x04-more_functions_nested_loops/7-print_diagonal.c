65ce2639d4c4e7e042376f317d846291d743873d
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 19:21:06 +0100

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,26 @@
 #include "main.h"
-
 /**
- * print_diagonal - prints a line
- * @n : is the number of times the character \\ should be printed
- * Return: nothing(void)
+ * print_diagonal - Print diagonal line dependent on the integer n.
+ * @n : The number of lines using '\' characters to use
+ * Return: Void.
  */
-
 void print_diagonal(int n)
 {
-int i, j, p;
-if (n > 0)
-{
-for (i = 0; i < n; i++)
-{
-p = i;
-for (j = 0; j < p; j++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
-_putchar('\n');
-}
-else
-{
-_putchar('\n');
-}
+	int i;
+	int spaces;
+
+	for (i = 0; i < n; i++)
+	{
+		for (spaces = 0; spaces < i; spaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
