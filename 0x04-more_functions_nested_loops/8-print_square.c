39def19146d5da8e3e133850792ce9738a4314f7
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 19:56:18 +0100

    Added a program to perint a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,24 +2,23 @@
 
 /**
  * print_square - Print square line dependent on the integer n.
- * @n : The number of lines using '#' characters to use per row and column
+ * @size : The number of lines using '#' characters to use per row and column
  * Return: Void.
  */
 void print_square(int size)
 {
-    int i;
-    int j;
-
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
+int i;
+int j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
 }
