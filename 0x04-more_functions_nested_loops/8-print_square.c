fd6c12eb02e6943804a5e3c98c2c396f2f232f38
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 19:53:19 +0100

    Added program to print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * print_square - prints a sqaure
- * @size : side of the square
- * Return: nothing(void)
+ * print_square - Print square line dependent on the integer n.
+ * @n : The number of lines using '#' characters to use per row and column
+ * Return: Void.
  */
-
 void print_square(int size)
 {
-int i;
-int j;
-for (j = 0; j < size; j++)
-{
-for (i = 0; i < size; i++)
-{
-_putchar('#');
-}	
-_putchar('\n');
-}
-if (size <= 0)
-{
-_putchar('\n');
-}
+    int i;
+    int j;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
 }
