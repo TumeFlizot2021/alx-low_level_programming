6727963b4e921a3b49706af512f46d110f40da34
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-18 21:04:04 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_triangle - check the code
+ *@size: determines the height of the triangle to be printed
  * Return: Always 0.
  */
 
@@ -11,7 +11,7 @@ void print_triangle(int size)
 int i, j, p;
 for (i = 0; i < size; i++)
 {
-for (j = size - i; j > 0; j--)
+for (j = size -1 - i; j > 0; j--)
 {
 _putchar(' ');
 }
