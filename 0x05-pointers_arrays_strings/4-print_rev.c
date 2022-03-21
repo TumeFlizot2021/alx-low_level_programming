173121d8f754ab25f60b34005987cd70c6f0a980
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-21 15:03:24 -0700

    IAdded function to perform string reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,19 +9,12 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	int len = 0;
 
-	while (*(s + i) != '\0')
-	{
-		len = len + 1;
-		i = i + 1;
-	}
+	while (s[i])
+		i++;
 
-	while (len > -1)
-	{
-		_putchar(*(s + len));
-		len = len - 1;
-	}
+	while (i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
