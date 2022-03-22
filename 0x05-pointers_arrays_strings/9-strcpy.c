6bd3dcafb10da4dffeece5cd3bf93938a80f567b
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-21 18:05:07 -0700

    Added string copy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - check string
+ * @des: string to be copied to
+ * @src : string to be copied from
+ * Return : Always return a pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+
+	return (dest);
+}
