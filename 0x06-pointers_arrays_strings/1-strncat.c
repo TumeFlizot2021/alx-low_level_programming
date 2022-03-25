3be4e53cb4606cf552c7513c26f9415bd2e9f886
TumeFlizot2021 <tumemichevflizot1002@gmail.com>
2022-03-25 07:41:56 -0700

    Added string catenate up to nth of src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - check the code.
+ *@dest : the address destination string.
+ *@src : the address tof the source string.
+ * @n : number of bytes to be copied from src
+ * Return: Always return destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr1;
+	char *ptr2;
+
+	int i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	ptr1 = dest + i;
+	ptr2 = src + n;
+
+	while (src < ptr2)
+		*ptr1++ = *src++;	
+
+	return (dest);	
+}
