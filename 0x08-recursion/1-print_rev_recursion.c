ed730c70f4ec3edaa7228db6bd3ffa74f79084d0
TumeFlizot2021 <85873948+TumeFlizot2021@users.noreply.github.com>
2022-03-30 13:20:31 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse order, followed by a new line
+ * _print_rev_recursion - prints a string in reverse order.
  * @s: pointer to the string
  * Return: void
  */
